bddb867273ff97014edfbb03b90e29ddc4cac947
Derek Kaknes <derek.kaknes@gmail.com>
2015-05-04 10:23:48 -0400

    README

--- calculations.rb
+++ calculations.rb
@@ -10,7 +10,7 @@ class Loan
     @monthly = options[:monthly]
   end
 
-  def payment
+  def monthly_payment
     if @monthly
       i = @rate / 12.0
       num_pmts = @term
@@ -30,7 +30,7 @@ class Loan
     else
       term = @term * 12
     end
-    (payment * term).round(2)
+    (monthly_payment * term).round(2)
   end
 
   def total_interest
@@ -43,15 +43,18 @@ class TestLoan < Test::Unit::TestCase
     a = Loan.new(10000,10,0.08)
     b = Loan.new(10000, 120, 0.08, {monthly: true})
     c = Loan.new(10000, 10, 0.08, {monthly: true})
-    assert_equal(a.payment, 121.33)
-    assert_equal(b.payment, 121.33)
-    assert_equal(c.payment, 1037.03)
+    assert_equal(a.monthly_payment, 121.33)
+    assert_equal(b.monthly_payment, 121.33)
+    assert_equal(c.monthly_payment, 1037.03)
   end
 
   def test_interest_cost
     d = Loan.new(5000, 10, 0.10)
+    e = Loan.new(10000, 10, 0.08)
     assert_equal(d.total_cost, 7929.6)
     assert_equal(d.total_interest, 2929.6)
+    assert_equal(e.total_cost, 14559.6)
+    assert_equal(e.total_interest, 4559.6)
   end
 end
 
